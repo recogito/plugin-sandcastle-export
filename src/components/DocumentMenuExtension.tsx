93278673e11520f1e38dd9592431c7be45afa83f
Rainer Simon <rainer@rainersimon.io>
2025-02-20 10:07:01 +0100

    Minor fix

--- src/components/DocumentMenuExtension.tsx
+++ src/components/DocumentMenuExtension.tsx
@@ -4,12 +4,10 @@ interface DocumentMenuExtensionProps {
 
 }
 
-const DocumentMenuExtension = (props: DocumentMenuExtensionProps) => {
+export const DocumentMenuExtension = (props: DocumentMenuExtensionProps) => {
 
   return (
     <a href={`/api/${props.projectId}/export/unreal`}>Export for Unreal 3D</a>
   )
 
 }
-
-export default DocumentMenuExtension;
