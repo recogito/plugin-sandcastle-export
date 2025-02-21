ba30e1584f9017e9994e4a189f33fe64b1c3f4ac
Rainer Simon <rainer@rainersimon.io>
2025-02-21 11:39:41 +0100

    Updated to new plugin registration model

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,48 @@
-import type { AstroIntegration } from "astro";
-import { registerExtensions } from "@recogito/studio-sdk";
+import type { AstroIntegration } from 'astro';
+import { Plugin, registerPlugin } from '@recogito/studio-sdk';
 
-const DocumentMenuExtension = {
-  name: "download-formats-document-menu-items",
-  component_name: "DocumentMenuExtension",
-  module_name: "@recogito/plugin-duke-unreal-export",
-  extension_point: "*",
+const DukeUnrealPlugin: Plugin = {
+
+  name: 'Duke Unreal Export',
+
+  description: 'A custom annotation export for Duke University\'s Unreal 3D importer',
+
+  author: 'Performant Software',
+
+  homepage: 'https://www.performantsoftware.com/',
+
+  extensions: [{
+
+    name: 'download-formats-document-menu-items',
+
+    component_name: 'DocumentMenuExtension',
+
+    module_name: '@recogito/plugin-duke-unreal-export',
+
+    extension_point: '*'
+
+  }]
+
 };
 
-const Plugin = (): AstroIntegration => ({
-  name: "unreal-export-plugin",
+const plugin = (): AstroIntegration => ({
+  name: 'unreal-export-plugin',
   hooks: {
-    "astro:config:setup": ({ config, logger, injectRoute }) => {
+    'astro:config:setup': ({ config, logger, injectRoute }) => {
       // UI: register additional download menu items
-      registerExtensions(DocumentMenuExtension, config, logger);
+      registerPlugin(DukeUnrealPlugin, config, logger);
 
       // Inject download route
-      logger.info("Injecting new API route: /api/[projectId]/export/unreal");
+      logger.info('Injecting new API route: /api/[projectId]/export/unreal');
 
       injectRoute({
-        pattern: "/api/[projectId]/export/unreal",
+        pattern: '/api/[projectId]/export/unreal',
         entrypoint:
-          "node_modules/@recogito/plugin-duke-unreal-export/src/api/UnrealExportRoute.ts",
+          'node_modules/@recogito/plugin-duke-unreal-export/src/api/UnrealExportRoute.ts',
         prerender: false,
       });
     },
   },
 });
 
-export default Plugin;
+export default plugin;
